16c0002f9ed6ca26d3f2709de13cdae455f239a5
Ricky <rky.w.liu@gmail.com>
2024-08-07 22:07:42 +1000

    Added gildedRoseKata

---
+++ GildedRoseKata/GildedRose/GildedRose.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GildedRoseKata;
+
+public class GildedRose
+{
+    IList<Item> Items;
+
+    public GildedRose(IList<Item> Items)
+    {
+        this.Items = Items;
+    }
+
+    public void NewUpdateQuality()
+    {
+        Items = Items.Select(item =>
+                item switch
+                {
+                    Item Conjured when Conjured.Name.Contains("Conjured") =>
+                        UpdateConjuredItem(item),
+                    Item Aged_Brie when Aged_Brie.Name.Contains("Aged Brie") =>
+                        UpdateAgedBrie(item),
+                    Item Backstage_Pass when Backstage_Pass.Name.Contains("Backstage passes") =>
+                        UpdateBackstagePass(item),
+                    Item Sulfuras when Sulfuras.Name.Contains("Sulfuras") =>
+                        UpdateLegendaryItem(item),
+                    Item _ => 
+                        UpdateStandardItem(item)
+                }).ToList();
+    }
+
+    private Item UpdateAgedBrie(Item item)
+    {
+        item.Quality = (item.Quality, item.SellIn) switch
+        {
+            (< 50, > 0) => int.Min(50, item.Quality + 1),
+            (< 50, <= 0) => int.Min(50, item.Quality + 2),
+            (50, _) => 50,
+            (_) => throw new Exception()
+        };
+
+        item.SellIn -= 1;
+        return item;
+    }
+
+    private Item UpdateBackstagePass(Item item)
+    {
+        item.Quality = item.SellIn switch
+        {
+            ( <= 0) => 0,
+            ( <= 5) => int.Min(50, item.Quality + 3),
+            ( <= 10) => int.Min(50, item.Quality + 2),
+            (_) => int.Min(50, item.Quality+1)
+        };
+        item.SellIn -= 1;
+        return item;
+    }
+
+    private Item UpdateLegendaryItem(Item item) => item;
+
+    private Item UpdateConjuredItem(Item item)
+    {
+        // Unfortunately, since api is not functional - we have to modify in place 
+        item.Quality = (item.SellIn, item.Quality) switch
+        {
+            (_, 0) => item.Quality,
+            ( > 0, _) => int.Max(0, item.Quality - 2),
+            ( <= 0, _) => int.Max(0, item.Quality - 4)
+        };
+        item.SellIn -= 1;
+
+        return item;
+    }
+
+    private Item UpdateStandardItem(Item item)
+    {
+        // Unfortunately, since api is not functional - we have to modify in place 
+        item.Quality = (item.SellIn, item.Quality) switch
+        {
+            (_, 0) => item.Quality,
+            (> 0, _) => int.Max(0, item.Quality - 1),
+            (<= 0, _ ) => int.Max(0, item.Quality - 2)
+        };
+        item.SellIn -= 1;
+
+        return item;
+    }
+
+
+    public void UpdateQuality()
+    {
+        for (var i = 0; i < Items.Count; i++)
+        {
+            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Items[i].Quality > 0)
+                {
+                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Items[i].Quality = Items[i].Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+    
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Items[i].SellIn < 11)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+    
+                        if (Items[i].SellIn < 6)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+    
+            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            {
+                Items[i].SellIn = Items[i].SellIn - 1;
+            }
+    
+            if (Items[i].SellIn < 0)
+            {
+                if (Items[i].Name != "Aged Brie")
+                {
+                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Items[i].Quality > 0)
+                        {
+                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GildedRoseKata/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,142 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void UpdateQuality_StandardItem_DegradesToZero()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 4 } };
+        GildedRose app = new GildedRose(Items);
+        
+
+        // Act/assert
+        app.NewUpdateQuality();
+        Assert.Equal(3, Items[0].Quality);
+        Assert.Equal(1, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(2, Items[0].Quality);
+        Assert.Equal(0, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+        Assert.Equal(-1, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+        Assert.Equal(-2, Items[0].SellIn);
+
+    }
+
+    [Fact]
+    public void UpdateQuality_Sulfurus_DoesNotDegrade()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras", SellIn = 2, Quality = 80 } };
+        GildedRose app = new GildedRose(Items);
+
+
+        // Act/assert
+        app.NewUpdateQuality();
+        Assert.Equal(80, Items[0].Quality);
+        Assert.Equal(2, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(80, Items[0].Quality);
+        Assert.Equal(2, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(80, Items[0].Quality);
+        Assert.Equal(2, Items[0].SellIn);
+
+    }
+
+    [Fact]
+    public void UpdateQuality_BackStagePass_Increases()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes", SellIn = 11, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+
+
+        // Act/assert
+        app.NewUpdateQuality();
+        Assert.Equal(1, Items[0].Quality);
+        Assert.Equal(10, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(3, Items[0].Quality);
+        Assert.Equal(9, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        app.NewUpdateQuality();
+        app.NewUpdateQuality();
+        app.NewUpdateQuality();
+
+        app.NewUpdateQuality();
+        Assert.Equal(14, Items[0].Quality);
+        Assert.Equal(4, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        app.NewUpdateQuality();
+        app.NewUpdateQuality();
+        app.NewUpdateQuality();
+        app.NewUpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+        Assert.Equal(-1, Items[0].SellIn);
+
+    }
+
+    [Fact]
+    public void UpdateQuality_AgedBrie_Increases()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 48 } };
+        GildedRose app = new GildedRose(Items);
+
+        // Act/assert
+        app.NewUpdateQuality();
+        Assert.Equal(49, Items[0].Quality);
+        Assert.Equal(0, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(50, Items[0].Quality);
+        Assert.Equal(-1, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(50, Items[0].Quality);
+        Assert.Equal(-2, Items[0].SellIn);
+    }
+
+    [Fact]
+    public void UpdateQuality_ConjuredItem_DegradesToZero()
+    {
+        // Arrange
+        // Arrange
+        IList<Item> Items = new List<Item> { new Item { Name = "Conjured foo", SellIn = 2, Quality = 7 } };
+        GildedRose app = new GildedRose(Items);
+
+
+        // Act/assert
+        app.NewUpdateQuality();
+        Assert.Equal(5, Items[0].Quality);
+        Assert.Equal(1, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(3, Items[0].Quality);
+        Assert.Equal(0, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+        Assert.Equal(-1, Items[0].SellIn);
+
+        app.NewUpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+        Assert.Equal(-2, Items[0].SellIn);
+    }
+}
