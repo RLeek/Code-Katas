47e683e0decb8c96dd2be4d3d2031ca10a2824df
Ricky <rky.w.liu@gmail.com>
2024-08-13 22:50:59 +1000

    Finished calculator kata

--- StringCalculatorKata/StringCalculatorKata/Calculator.cs
+++ StringCalculatorKata/StringCalculatorKata/Calculator.cs
@@ -10,59 +10,127 @@ namespace StringCalculatorKata
 {
     public class Calculator
     {
+        private readonly Regex numberRegex = new Regex("(^[0-9]+)");
+        private readonly Regex negativeNumberRegex = new Regex("(^-[0-9]+)");
+        private readonly Regex defaultDelimiterRegex = new Regex(@"^//([^\[\]\n]+)\n");
+        private readonly Regex multiDelimiterRegex = new Regex(@"\[([^\[\]]+)\]");
+        private readonly Regex startRegex = new Regex(@"//");
+
+
+        private Regex delimiterRegex = new Regex($"(^[,\n])");
+        private Regex validCharactersRegex = new Regex("^[0-9,\n]+$");
+
+        private Regex newDelimiterRegex(List<string> delimiter) => new Regex(@$"^({(string.Join('|', delimiter.Select(x=>Regex.Escape(x)).ToList()))}|\n)");
+
+
+        private char delimiter = ',';
+
+
         public int Add(string numbers)
         {
-            if (!isValid(numbers))
-            {
-                throw new Exception("Unexpected input");
-            }
-
             if (numbers == string.Empty)
             {
                 return 0;
             }
-         
-            var numberRegex = new Regex("[0-9]*");
-            var matches = numberRegex.Matches(numbers);
-            var sum = 0;
-            foreach (var match in matches.ToList())
+
+            if (numbers[numbers.Count() - 1] > '9' && numbers[numbers.Count() -1] < '0' )
             {
-                if (match.Value!= string.Empty)
-                {
-                    sum+= Convert.ToInt32(match.Value.ToString());
-                }
+                throw new Exception("Invalid argument");
             }
+
+            var match = MatchDefaultDelimiter(numbers);
+            if (match.Count != 0)
+            {
+                delimiterRegex = newDelimiterRegex(match);
+                numbers = numbers.Substring(numbers.IndexOf('\n')+1);
+            }
+
+            var negativeNumbers = new List<int>();
+            var sum = 0;
+            while(numbers.Length > 0)
+            {
+                var number = MatchNumber(numbers);
+                var parsedNumber = Convert.ToInt32(number);
+                if (parsedNumber < 0)
+                {
+                    negativeNumbers.Add(parsedNumber);
+                }
+                if (parsedNumber <= 1000)
+                {
+                    sum += Convert.ToInt32(parsedNumber);
+                }
+                numbers = numbers.Substring(number.Count());
+                if (numbers.Length == 0)
+                {
+                    // we could switch this to be a toggle based on the previous token
+                    // That would generalize better but ehh
+                    break;
+                }
+                var delimiter = MatchDelimiter(numbers);
+                numbers = numbers.Substring(delimiter.Count());
+            }
+
+            if (negativeNumbers.Count > 0)
+            {
+                throw new Exception($"negatives not allowed {string.Join(',', negativeNumbers)}");
+            }
+
 
             return sum;
         }
 
-        private bool isValid(string numbers)
+        private List<string> MatchDefaultDelimiter(string input)
         {
-            if (numbers.Length == 0)
+            var delimiters = new List<string>();
+            if (startRegex.IsMatch(input))
             {
-                return true;
-            }
-
-            var validCharactersRegex = new Regex("^[0-9,]*$");
-            if (!validCharactersRegex.IsMatch(numbers))
+                if (defaultDelimiterRegex.IsMatch(input))
+                {
+                    var value = defaultDelimiterRegex.Match(input);
+                    delimiters.Add(defaultDelimiterRegex.Match(input).Groups[1].ToString());
+                    return delimiters;
+                } else
+                {
+                    input = input.Substring(2);
+                    while (input[0] != '\n')
+                    {
+                        var match = multiDelimiterRegex.Match(input).Value;
+                        if (match.Count() == 0)
+                        {
+                            throw new Exception("Don't make sense");
+                        }
+                        delimiters.Add(match.TrimStart('[').TrimEnd(']'));
+                        input = input.Substring(match.Count());
+                    }
+                    return delimiters;
+                }
+            } else
             {
-                return false;
+                return delimiters;
             }
-
-            // Invalid sequence is any sequence of ',,' or longer
-            var validSequenceRegex = new Regex(",,");
-            if (validSequenceRegex.IsMatch(numbers))
-            {
-                return false;
-            }
-
-            if (numbers.First() == ',' || numbers.Last() == ',')
-            {
-                return false;
-            }
-
-            return true;
         }
 
+        private string MatchDelimiter(string input)
+        {
+            if (delimiterRegex.IsMatch(input))
+            {
+                return delimiterRegex.Match(input).Value;
+            }
+
+            throw new Exception("received incorrect token");
+        }
+
+        private string MatchNumber(string input)
+        {
+            if (numberRegex.IsMatch(input))
+            {
+                return numberRegex.Match(input).Value;
+            }
+            if (negativeNumberRegex.IsMatch(input))
+            {
+                return negativeNumberRegex.Match(input).Value;
+            }
+            throw new Exception("received incorrect token");
+        }
     }
 }
--- StringCalculatorKata/StringCalculatorTests/CalculatorTests.cs
+++ StringCalculatorKata/StringCalculatorTests/CalculatorTests.cs
@@ -30,27 +30,100 @@ namespace StringCalculatorTests
             Assert.Equal(1, result);
         }
 
-        [Fact]
-        public void TwoNumString_ReturnsSum()
+        [Theory]
+        [InlineData("1,1")]
+        [InlineData("1\n1")]
+        [InlineData("//;\n1;1")]
+        [InlineData("//;\n1\n1")]
+        public void TwoNumString_ReturnsSum(string input)
         {
             // Arrange
             var calculator = new Calculator();
 
             // Act
-            var result = calculator.Add("1,1");
+            var result = calculator.Add(input);
 
             // Assert
             Assert.Equal(2, result);
         }
 
-        [Fact]
-        public void ThreeNumString_ReturnsSum()
+        [Theory]
+        [InlineData("1,1,1")]
+        [InlineData("1\n1,1")]
+        [InlineData("1\n1\n1")]
+        [InlineData("//;\n1;1;1")]
+        [InlineData("//;\n1;1\n1")]
+        public void ThreeNumString_ReturnsSum(string input)
         {
             // Arrange
             var calculator = new Calculator();
 
             // Act
-            var result = calculator.Add("1,1,1");
+            var result = calculator.Add(input);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+
+        [Theory]
+        [InlineData("1,-1,1")]
+        [InlineData("-1\n1,1")]
+        [InlineData("1\n1\n-1")]
+        [InlineData("//;\n-1;1;1")]
+        [InlineData("//;\n1;-1\n1")]
+        public void NegativeNumString_ThrowsException(string input)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            Action result = ()=> calculator.Add(input);
+
+            // Assert
+            Assert.Throws<System.Exception>(result);
+        }
+
+
+        [Theory]
+        [InlineData("1001,1")]
+        [InlineData("1,1001,1001")]
+        public void LargeNumString_IgnoresNum(string input)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(input);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+        [Theory]
+        [InlineData("//******\n1******1")]
+        [InlineData("//sad\n1sad1")]
+        public void CustomDelimiterString_ReturnsSum(string input)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(input);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Theory]
+        [InlineData("//[******][sad]\n1******1sad1")]
+        [InlineData("//[gg][sad]\n1gg1sad1")]
+        public void MultipleDelimiterString_ReturnsSum(string input)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var result = calculator.Add(input);
 
             // Assert
             Assert.Equal(3, result);
